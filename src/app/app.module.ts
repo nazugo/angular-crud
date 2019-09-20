dccee8d4a93b15559bd39ea6be6ae58b7700259a
nazugo <45163232+nazugo@users.noreply.github.com>
2019-09-20 11:50:34 +0200

    Etape 9

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,8 @@ import { ProductEditComponent } from './product-edit/product-edit.component';
 
 import { SlimLoadingBarModule } from 'ng2-slim-loading-bar';
 import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { ProductsService } from './products.service';
 
 @NgModule({
   declarations: [
@@ -21,7 +23,9 @@ import { ReactiveFormsModule } from '@angular/forms';
     BrowserModule,
     AppRoutingModule,
     SlimLoadingBarModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule,
+    ProductsService
   ],
   providers: [],
   bootstrap: [AppComponent]
